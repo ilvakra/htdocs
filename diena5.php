b3453b48320f6626f66f0d5fbffd4df16282b7b2
Ilva Linde <ilva.linde@delfi.lv>
2018-06-09 11:21:15 +0300

    public static function

--- diena5.php
+++ diena5.php
@@ -5,6 +5,7 @@ class Device{
 
 	public $year;
 	public $manufacturer;
+	public static $all_manufacturers;
 	
 	public function getYear(){
 		return $this->year;
@@ -24,6 +25,14 @@ class Device{
 		return ($this->company . " " . $this->year);
 	}
 
+	public static function getAllManufacturers(){
+		
+		//db?
+
+		return ["Siemens", "Bosch", "Apple", "Audi"];
+		// return array("Siemens", "Bosch", "Apple", "Audi");
+	}
+
 }
 
 
@@ -31,5 +40,7 @@ $device = new Device();
 $device->setYear(2017);
 $device->setManufacturer("Siemens");
 print($device->getFullInfo());
+var_dump(Device::getAllManufacturers());
+
 
 
