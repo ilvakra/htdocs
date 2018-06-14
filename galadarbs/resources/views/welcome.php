c7eb1eee75329b2dcb12c77b43b6a678da55ac13
Ilva Linde <ilva.linde@delfi.lv>
2018-06-14 17:58:59 +0300

    magic while talking about Adam

--- galadarbs/resources/views/welcome.php
+++ galadarbs/resources/views/welcome.php
@@ -51,9 +51,25 @@ $navigation=['welcome'=>'/galadarbs/public', 'about'=>'/diena1.php', 'contact'=>
 
 <main class="container bg-warning">
 
+    <div class="btn btn-success btn-lg d-block">Yay!</div>
+
+
 
     <div class="row">
-        <div class="col-12 bg-info col-md-4 col-lg-2">
+        <div class="col-12 bg-info col-md-4 col-lg-2 col-xl-6 ">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <div class="row">
+                <div class="col-4">aaaa</div>
+                <div class="col-4">bbb</div>
+                <div class="col-4">cccc</div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4  col-lg-2 offset-lg-3 col-xl-6 offset-xl-0 bg-success">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -61,15 +77,7 @@ $navigation=['welcome'=>'/galadarbs/public', 'about'=>'/diena1.php', 'contact'=>
             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
-        <div class="col-12 col-sm-6 col-md-4  col-lg-2 offset-lg-3 bg-success">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-        <div class="col-12 col-sm-6 col-md-4  col-lg-2 offset-lg-3 bg-primary">
+        <div class="col-12 col-sm-6 col-md-4  col-lg-2 offset-lg-3 col-xl-6 offset-xl-0 bg-primary d-md-none">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -78,7 +86,6 @@ $navigation=['welcome'=>'/galadarbs/public', 'about'=>'/diena1.php', 'contact'=>
             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
     </div>
-
     
     <article>
         <header></header>
@@ -86,7 +93,7 @@ $navigation=['welcome'=>'/galadarbs/public', 'about'=>'/diena1.php', 'contact'=>
     </article>
 </main>
 
-<footer class="container-fluid bg-dark text-light text-center">
+<footer class="container-fluid bg-dark text-light text-center fixed-bottom">
     2018
 </footer>
 
