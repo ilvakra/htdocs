37775304e5e7b45f582290bbc621a0f125fc4c34
Ilva Linde <ilva.linde@delfi.lv>
2018-06-14 17:17:59 +0300

    navigation to bootstrap

--- galadarbs/resources/views/welcome.php
+++ galadarbs/resources/views/welcome.php
@@ -9,97 +9,68 @@
 
 <body>
 
-    <img class="w-50" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfO3N7VeJgDHJr_7JWTVycrUInYuKxKclzxgYWaQPc2N0WdvKV" alt="Third slide">
 
-
-    <div id="mansKaruselis" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#mansKaruselis" data-slide-to="0" class="active"></li>
-        <li data-target="#mansKaruselis" data-slide-to="1"></li>
-        <li data-target="#mansKaruselis" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img class="d-block w-100" src="https://qph.fs.quoracdn.net/main-qimg-92822f563a92ee06c81c6e7f1d7a2b0c" alt="First slide">
-      </div>
-      <div class="carousel-item">
-          <img class="d-block w-100" src="https://i.ytimg.com/vi/v5KzBPUEgGQ/maxresdefault.jpg" alt="Second slide">
-      </div>
-      <div class="carousel-item">
-          <img class="d-block w-100" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfO3N7VeJgDHJr_7JWTVycrUInYuKxKclzxgYWaQPc2N0WdvKV" alt="Third slide">
-      </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-</a>
-<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-</a>
-</div>
-
-
-
-<div class="alert alert-success" role="alert">
-  <h4 class="alert-heading">Well done!</h4>
-  <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
-  <hr>
-  <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
-</div>
-<button type="button" class="btn btn-warning">Warning</button>
-
-aaa bbbb <span class="d-block m-5 bg-dark py-5 px-3 ">ccc</span> ddd
-
-<table class="table table-striped bg-warning text-white text-center">
-    <tr>
-        <td>1</td>
-        <td>2</td>
-    </tr>
-    <tr>
-        <td>1</td>
-        <td>2</td>
-    </tr>
-    <tr>
-        <td>1</td>
-        <td>2</td>
-    </tr>
-    <tr>
-        <td>1</td>
-        <td>2</td>
-    </tr>
-</table>
-
-
-<?php
+<header class="bg-dark text-light">
+        <?php
 $navigation=['welcome'=>'/galadarbs/public', 'about'=>'/diena1.php', 'contact'=>'/diena1.php'];
 ?>
-<nav>
-    <ul>
-     <?php
+    
+   
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-     foreach ($navigation as $name => $url) 
-     {
-        echo("<li><a href='$url'>$name</a></li>");
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
 
-            // echo('<li><a href="$url">$name</a></li>');
+        <?php
 
-            // echo('<li><a href="'.$url.'">'.$name.'</a></li>');
-    }
-    ?> 
-</ul>
+         foreach ($navigation as $name => $url) 
+             {
+                echo('
+                    <li class="nav-item active">
+                        <a class="nav-link" href="'.$url.'">'.$name.'</a>
+                    </li>
+                  ');
+                
+        } ?> 
+
+    
+    </ul>
+  </div>
 </nav>
 
-<div class="container">
 
-</div>
+</header>
+
+<main class="container bg-warning">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <article>
+        <header></header>
+        <main></main>
+    </article>
+</main>
+
+<footer class="container-fluid bg-dark text-light text-center">
+    2018
+</footer>
 
 
 
 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
