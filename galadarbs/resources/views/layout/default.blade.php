d70df6d717f7ee50b64fe88972497e912cd89827
Ilva Linde <ilva.linde@delfi.lv>
2018-06-20 16:30:23 +0300

    jquery

--- galadarbs/resources/views/layout/default.blade.php
+++ galadarbs/resources/views/layout/default.blade.php
@@ -46,70 +46,26 @@
 
 
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     <script type="text/javascript">
 
-        // var data = 1;
+        // document.getElementById('navbarSupportedContent').innerHTML = "1";
+        $('#navbarSupportedContent').html(1);
 
-        // console.log(data);
-        // var data = "text";
-        // console.log(data);
-        // var data = {type:"Fiat", model:"500", color:"white"};
-        // console.log(data);
-        // alert(data);
-        //alert(1);
-        
-        function sayHello(name){
-            console.log("hello!" + name);
-        }
-        sayHello("Mark");
+        $('div').html('this is a div!');
+        $('div').addClass('bg-dark');
+        setInterval(function(){
 
-        // var paragraphs = document.getElementsByTagName("div");
-        // console.log(paragraphs);
+         $('div').toggleClass('bg-dark');
+        }, 2000);
 
-        var fourColumns = [ document.getElementsByTagName('li') ];
-        console.log(fourColumns);
-        fourColumns.forEach(function(element) {
-          console.log("element: "  + element);
-        });
-
-        for(i = 0; i < fourColumns.length; i++)
-        {
-            console.log("This also works: " + fourColumns[i].value);
-        }
-
-        // var element = document.getElementById('navbarSupportedContent');
-        // console.log(element);
-
-        // setTimeout( 
-        //     function(){
-        //         element.innerHTML = "magic";
-        //     }, 
-        //     3000
-        // );
-
-        // // var flag = 0;
-        // var i = 0;
-        // var myInterval = setInterval( 
-        //     function(){
-        //         console.log(1);
-        //         // if (flag == false){
-        //         //     element.innerHTML = i++;
-        //         // }
-        //         element.innerHTML = i++;
-        //     }, 
-        //     1000
-        // );
-
-        // // console.log(document.getElementsByClassName("btn")[0]);
-        // document.getElementsByClassName("btn")[0].addEventListener("click", function(){
-        //         // flag=1;
-        //         clearInterval(myInterval);
-        // });
-
+        console.log($('#navbarSupportedContent'));
     </script>
 </body>
 
